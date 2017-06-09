2130c00553aaa3592a074c738a17c732ef6826cf
Ondrej Kohout <ondrej.kohout@gmail.com>
2017-06-09 21:29:58 +0100

    whoops, add forgotten conftest

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import redis
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--test-redis-uri',
+        action='store',
+        dest='TEST_REDIS_URI',
+        default='redis://localhost:6379/11',
+        help='Redis uri for testing (e.g. "redis://localhost:6379/11")')
+
+
+@pytest.fixture
+def redis_(request):
+    client = redis.StrictRedis.from_url(
+        request.config.getoption('TEST_REDIS_URI'))
+    yield client
+    client.flushdb()
