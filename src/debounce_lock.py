1ab585c62f742175d2fa6151464b823265b4a4e9
Ondrej Kohout <ondrej.kohout@gmail.com>
2017-07-11 08:58:46 +0800

    This will deserve better wording

--- src/debounce_lock.py
+++ src/debounce_lock.py
@@ -14,11 +14,6 @@ class Lock:
     format_key = 'lock:{}'.format
 
     def acquire(self, key):
-        """ Try to acquire a lock and return `True` on success
-
-        Returns `False` if the lock is already acquired by someone else
-
-        """
         key = self.format_key(key)
         pipe = self.client.pipeline()
         pipe.incr(key)
@@ -27,12 +22,6 @@ class Lock:
         return count <= 1
 
     def release(self, key):
-        """ Release the lock
-
-        Returns `True` if somebody else tried to acquire the same lock during
-        the time it was held. Otherwise returns `False`.
-
-        """
         count = self.client.getset(self.format_key(key), 0)
         count = int(count) if count else 0
         return count > 1
